6d8f4a5f20df8fabecd199c10438c7734137f399
joeypercia <jperc004@ucr.edu>
2021-04-13 13:25:53 -0700

    Added classes, modified op.hpp

---
+++ pow.hpp
@@ -0,0 +1,29 @@
+fndef __POW_HPP__
+#define __POW_HPP__
+
+#include "base.hpp"
+#include <cmath> // for pow
+
+class Op : public Base {
+private:
+    double _value;
+    double _lop;
+    double _rop;
+public:
+    
+    Op(double lop, double rop) : _value(std::pow(lop, rop))
+    {
+        this->_lop = lop;
+        this->_rop = rop;
+    };
+    virtual double evaluate() { return this->_value; };
+    virtual std::string stringify() {
+        std::string valuetostring1 = std::to_string(_lop);
+        std::string valuetostring2 = std::to_string(_rop);
+        std::string valuetostring3 = "(" + valuetostring1 + "/" + valuetostring2 + ")";
+        return valuetostring3;
+    };
+};
+
+#endif //__POW_HPP__
+#pragma once
