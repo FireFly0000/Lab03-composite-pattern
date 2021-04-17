2c190e59b4b07ff176ac30bd64049b89751512c1
FireFly0000 <wktfirefly283@gmail.com>
2021-04-16 20:57:53 -0700

    changing comment in op.hpp

--- op.hpp
+++ op.hpp
@@ -8,7 +8,7 @@ class Op : public Base {
 private: 
     double _value;
 public:
-    Op(double value) : _value(value) { }; // not sure, may have to fix
+    Op(double value) : _value(value) { };
     virtual double evaluate() { return this->_value; };
     virtual std::string stringify() { 
         std::string valuetostring = std::to_string(_value);
